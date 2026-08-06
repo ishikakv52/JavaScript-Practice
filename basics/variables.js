f658ccc86dda16e35ee3b5ada2a5847a9c17c9ba
Ishika <ishikakv52@gmail.com>
2026-08-06 09:53:22 +0000

    let,var,const

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId=145455
+let accountEmail="rathi123@gmail.com"
+let accountPassword="rathi@123"
+accountCity="Bangalore"
+// accountId=2.  # not allowed
+accountEmail="ishika@gmail.com"
+accountPassword="ishika@123"
+accountCity="Delhi"
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
