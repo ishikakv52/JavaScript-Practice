3cc81f2a12b3182877e8883f7d5a7d1d5296c0ca
Ishika <ishikakv52@gmail.com>
2026-08-06 10:15:20 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as newer version
+let name="Ishika"
+let age=18
+let isLoggedin= false
+//Primitive :
+//Number
+//Bigint
+//String=" "
+// Boolean
+//null standalone value
+//undefined
+//symbol=> unique
+
+
+//Objects:
+console.log(typeof null)
