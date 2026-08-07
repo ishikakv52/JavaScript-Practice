1d13fbfdcd6cb4aecf15521035a0c8739b81ba32
Ishika <ishikakv52@gmail.com>
2026-08-07 13:31:30 +0000

    numbers and maths

---
+++ basics/numbers.js
@@ -0,0 +1,21 @@
+// node basics/numbers.js
+let bal=100.9899
+// console.log(bal)
+// console.log(typeof bal)
+// console.log(bal.toString().length)
+// console.log(bal.toPrecision(5))    //Total  significant digits
+const hundreds=1000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+
+
+// ---------Maths----------------
+// console.log(Math)
+// console.log(Math.abs(-4));  //always make positive
+// console.log(Math.round(4.559)); // round off
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
