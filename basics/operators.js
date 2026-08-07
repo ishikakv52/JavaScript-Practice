7efabfabb2d61de928b05d7ca131e3604bd0d1b5
Ishika <ishikakv52@gmail.com>
2026-08-07 04:24:12 +0000

    Operators

---
+++ basics/operators.js
@@ -0,0 +1,23 @@
+let value=3
+let negvalue=-value
+// console.log(negvalue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**3);
+
+let str1="Ishika"
+let str2="Rathi"
+str3=str1+str2
+console.log(str1+str2);
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(value++);
+console.log(++value);
+console.log(--value);
+console.log(value--);
