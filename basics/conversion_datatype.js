2ecca0c5c7849be933ecfd8fe5f7b6a77066f95e
Ishika <ishikakv52@gmail.com>
2026-08-07 04:12:50 +0000

    Datatypes Conversion

---
+++ basics/conversion_datatype.js
@@ -0,0 +1,14 @@
+let score="100ABC"
+console.log(typeof score);
+let ValueInNumber=Number(score)
+console.log(ValueInNumber);
+console.log(typeof ValueInNumber)
+//Nan=> Not a Number
+//"33" => 33
+//"33abc"=> NaN
+//True=>1, False => 0
+
+
+let IsLogged=1
+let boolcnvrt=Boolean(IsLogged)
+console.log(boolcnvrt)
