1faa5d92d228c87700ebe9851199abcd338a09f1
Ishika <ishikakv52@gmail.com>
2026-08-08 05:56:14 +0000

    local and global scope

---
+++ basics3/scopes.js
@@ -0,0 +1,17 @@
+//node basics3/scopes.js
+
+//global scope
+// let c=300
+
+//local scope👇🏻
+if (true){
+
+    let a=10
+    const b=20
+    // var c=30
+    //or
+   c=30
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
