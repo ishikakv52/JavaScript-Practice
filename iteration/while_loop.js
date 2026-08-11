780474d60d2d11bd73cad88e8a0fd41d2e9aabda
Ishika <ishikakv52@gmail.com>
2026-08-11 06:50:46 +0000

    While and do-while loop

---
+++ iteration/while_loop.js
@@ -0,0 +1,18 @@
+//node iteration/while_loop.js
+let i=0
+while(i<10){
+    // console.log(`Value of index is ${i}`);
+    i=i+2;
+}
+let myArray=["J","A","A","T"]
+let arr=0
+while(arr<myArray.length){
+    // console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+let score =1;
+do{
+    console.log(`Score is ${score}`)
+    score++
+}
+while(score<=10);
