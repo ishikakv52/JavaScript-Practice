339c666a4390a7f52252d641c62a2d9a27d4b2f2
Ishika <ishikakv52@gmail.com>
2026-08-11 11:56:38 +0000

    for of loop on strings

--- iteration/for.of.js
+++ iteration/for.of.js
@@ -1,6 +1,12 @@
 //node iteration/for.of.js
 //for of
+//for array
 const arr=[1,2,3,4,5]
 for (const i of arr){
     console.log(i)
+}
+//for string
+const greetings="Hello World!"
+for (let i of greetings){
+    console.log(`Each character is ${i}`)
 }
