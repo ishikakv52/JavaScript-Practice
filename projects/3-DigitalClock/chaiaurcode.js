8bd653bba32d344deff3c6164295f82cc7de6710
Ishika <ishikakv52@gmail.com>
2026-08-14 09:09:15 +0000

    deleted extra files

--- projects/3-DigitalClock/chaiaurcode.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("clock")
-// document.querySelector('#clock')
-setInterval(() => {
-let date=new Date()
-// console.log(date.toLocaleTimeString());
-clock.innerHTML=date.toLocaleTimeString();
-}, 1000);
